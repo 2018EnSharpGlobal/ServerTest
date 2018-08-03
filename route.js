78272d0fc0b1a55d206c508dbd09b905e37e3610
Jiho <giho-p@hanmail.net>
2018-08-03 20:40:04 +0900

    Express Framework test

---
+++ route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router(); //라우터 분리
+router.get('/', (req, res) => { //app대신 router에 연결
+     res.render('main');
+});
+router.get('/about', (req, res) => {
+     res.render('about');
+});
+module.exports = router; //모듈로 만드는 부분
